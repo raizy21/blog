e1f26c29761cf38d12eb62eec29b155e2c8a2778
Razvan Andrei <chiper_raizy@yahoo.com>
2025-05-17 07:06:37 +0200

    [code] adding a post router

--- routes/blog.router.js
+++ routes/blog.router.js
@@ -11,8 +11,19 @@ router.get("/posts", (req, res) => {
   res.status(200).render("posts-list");
 });
 
+router.post("/posts", async (req, res) => {
+  const { title, summary, body, author_id } = req.body;
+  await pool.query(
+    "INSERT INTO posts (title, summary, body, author_id) VALUES (?)",
+    [title, summary, body, author_id]
+  );
+
+  res.status(201).redirect("/posts");
+});
+
 router.get("/new-post", async (req, res) => {
   const [authors] = await pool.query("SELECT * FROM authors");
   res.status(200).render("create-post", { authors: authors });
 });
+
 export default router;
