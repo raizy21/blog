03fd1f241dbf8aef21a55e00e510c191df4ad555
Razvan Andrei <chiper_raizy@yahoo.com>
2025-05-19 07:06:03 +0200

    [code] adding a more readbale date for the time tag

--- routes/blog.router.js
+++ routes/blog.router.js
@@ -46,7 +46,18 @@ router.get("/posts/:id", async (req, res, next) => {
     return res.status(404).render("404");
   }
 
-  res.render("post-detail", { post: posts[0] });
+  const postData = {
+    ...posts[0],
+    date: posts[0].date.toISOString(),
+    humanReadableDate: posts[0].date.toLocaleDateString("en-US", {
+      weekday: "long",
+      year: "numeric",
+      month: "long",
+      day: "numeric",
+    }),
+  };
+
+  res.render("post-detail", { post: postData });
 });
 
 export default router;
